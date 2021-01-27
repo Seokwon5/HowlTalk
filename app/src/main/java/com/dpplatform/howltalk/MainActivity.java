18441212a24b35b2c676c3e68952b86f5a870fe7
LeeSeokWon <tjrdnjs5@gmail.com>
2021-01-28 01:30:01 +0900

    Initial commit

---
+++ app/src/main/java/com/dpplatform/howltalk/MainActivity.java
@@ -0,0 +1,65 @@
+package com.dpplatform.howltalk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.BuildConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.security.PrivateKey;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linear_splash;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linear_splash = (LinearLayout)findViewById(R.id.linear_splash);
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(R.xml.default_config);
+        mFirebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+
+                        } else {
+
+                        }
+                        displayMessage();
+                    }
+                });
+
+    }
+
+    void displayMessage() {
+        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = mFirebaseRemoteConfig.getString("splash_message");
+
+
+
+
+        }
+
+
+    }
+}
