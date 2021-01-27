4be94f751647a16f42b8d5c63c28ad894e258322
seokwon <tjrdnjs5@gmail.com>
2021-01-28 02:04:20 +0900

    내일 이거 이어가길 바람.

--- app/src/main/java/com/dpplatform/howltalk/MainActivity.java
+++ app/src/main/java/com/dpplatform/howltalk/MainActivity.java
@@ -55,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
         boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
         String splash_message = mFirebaseRemoteConfig.getString("splash_message");
 
-        //이거 내가 수정했어!!
-
 
 
 
