d3764871f1557488d8bf1879ac9b564695b3f109
seokwon <tjrdnjs5@gmail.com>
2021-01-30 20:55:24 +0900

    SignupActivity 생성. signin -> signup 으로 변수 변경.

---
+++ app/src/main/java/com/dpplatform/howltalk/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.dpplatform.howltalk;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button signup;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        getWindow().setStatusBarColor(Color.parseColor(splash_background));
+
+        login = (Button)findViewById(R.id.loginActivity_button_login);
+        signup = (Button)findViewById(R.id.loginActivity_button_signup);
+        login.setBackgroundColor(Color.parseColor(splash_background));
+        signup.setBackgroundColor(Color.parseColor(splash_background));
+
+
+
+    }
+}
