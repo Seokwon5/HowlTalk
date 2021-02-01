229dcfda38862980c97664a27272af73e7981c7a
seokwon <tjrdnjs5@gmail.com>
2021-02-01 19:20:28 +0900

    회원가입 창에 프로필 사진 넣기 성공. 빈값일때 imageUri == null 코드 삽입

--- app/src/main/java/com/dpplatform/howltalk/SignupActivity.java
+++ app/src/main/java/com/dpplatform/howltalk/SignupActivity.java
@@ -1,12 +1,18 @@
 package com.dpplatform.howltalk;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.dpplatform.howltalk.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,12 +20,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText name;
     private EditText password;
     private Button signup;
+    private ImageView profile;
+    private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +42,62 @@ public class SignupActivity extends AppCompatActivity {
         name = findViewById(R.id.signupActivity_editText_name);
         password = findViewById(R.id.signupActivity_editText_password);
         signup = findViewById(R.id.signupActivity_button_signup);
+        profile = findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
 
+        //가입 버튼 클릭했을때
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (email.getText().toString() == null || name.getText().toString() == null || password.getText().toString() == null)
+                if (email.getText().toString() == null || name.getText().toString() == null || password.getText().toString() == null
+                || imageUri == null) { //빈값일때,
                     return;
+                }
 
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
+                @Override
+                public void onComplete (@NonNull Task < AuthResult > task) {
+                    String uid = task.getResult().getUser().getUid();
+                    FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            String imageUrl = task.getResult().getUploadSessionUri().toString();
 
-                                UserModel userModel = new UserModel();
-                                userModel.userName = name.getText().toString();
-                                String uid = task.getResult().getUser().getUid();
+                            UserModel userModel = new UserModel();
+                            userModel.userName = name.getText().toString();
+                            userModel.profileImageUrl = imageUrl;
 
-                                FirebaseDatabase.getInstance().getReference().child("userName").child(uid).setValue(userModel);
+                            FirebaseDatabase.getInstance().getReference().child("userName").child(uid).setValue(userModel);
+
+
+                        }
+                    });
 
                             }
+
                         });
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK) {
+            profile.setImageURI(data.getData()); //profile이미지 바꿈.
+            imageUri = data.getData(); //이미지 경로 원본
+
+        }
     }
 }
--- app/src/main/java/com/dpplatform/howltalk/model/UserModel.java
+++ app/src/main/java/com/dpplatform/howltalk/model/UserModel.java
@@ -2,4 +2,5 @@ package com.dpplatform.howltalk.model;
 
 public class UserModel {
     public String userName;
+    public String profileImageUrl;
 }
