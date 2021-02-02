907f7d70d98f3ebb419963817d80c9c484a773eb
LeeSeokWon <tjrdnjs5@gmail.com>
2021-02-02 22:54:25 +0900

    로그인까지 완료.

--- app/src/main/java/com/dpplatform/howltalk/LoginActivity.java
+++ app/src/main/java/com/dpplatform/howltalk/LoginActivity.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class LoginActivity extends AppCompatActivity {
@@ -27,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button login;
     private Button signup;
     private FirebaseRemoteConfig FirebaseRemoteConfig;
-    private FirebaseAuth firebaseAuth; //로그인 관리
+    private FirebaseAuth firebaseAuth; //로그인 관리 툴
     private FirebaseAuth.AuthStateListener authStateListener; //로그인이 되었는지 확인해줌 (로그인이 되었으면 다음 화면으로 넘어가게 됨)
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -36,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut(); //앱이 꺼지면 자동 로그아웃.
 
         FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         String splash_background = FirebaseRemoteConfig.getString("splash_background");
@@ -49,7 +51,8 @@ public class LoginActivity extends AppCompatActivity {
         login.setBackgroundColor(Color.parseColor(splash_background));
         signup.setBackgroundColor(Color.parseColor(splash_background));
 
-        login.setOnClickListener(new View.OnClickListener() { //로그인 버튼을 눌렀을때,
+        //로그인 버튼을 눌렀을때,
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginEvent();
@@ -63,7 +66,27 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //로그인 인터페이스 리스너
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //로그인
+                    Intent intent = new Intent(LoginActivity.this , MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    //로그아웃
+                }
+
+            }
+        };
+
+
+
     }
+
     //로그인이 정상적으로 되었는지 확인해주는 메소드,
     void loginEvent() {
         firebaseAuth.signInWithEmailAndPassword(id.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -76,5 +99,17 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
     }
 }
