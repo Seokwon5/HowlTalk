530e1a8a547f5897f0e4267d9a27aa13805bbb44
seokwon <tjrdnjs5@gmail.com>
2021-02-04 02:01:05 +0900

    이미지뷰 안에 이미지 넣어야 함. PeopleFragment.java onCreateViewHolder 안에 넣기.

--- app/src/main/java/com/dpplatform/howltalk/MainActivity2.java
+++ app/src/main/java/com/dpplatform/howltalk/MainActivity2.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import fragment.PeopleFragment;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainactivity2_framelayout, new PeopleFragment()).commit();
     }
 }
---
+++ app/src/main/java/fragment/PeopleFragment.java
@@ -0,0 +1,97 @@
+package fragment;
+
+import android.app.VoiceInteractor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.dpplatform.howltalk.R;
+import com.dpplatform.howltalk.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.peoplefragment, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.peoplefragment_resyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+
+        return view;
+    }
+    //어뎁터
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        List<UserModel> userModels;
+        public PeopleFragmentRecyclerViewAdapter() {
+            userModels = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    userModels.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        userModels.add(dataSnapshot.getValue(UserModel.class));
+                        notifyDataSetChanged();
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friends, parent, false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return userModels.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+
+            public ImageView imageView;
+            public TextView textView;
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.itemfriend_imageview);
+                textView = view.findViewById(R.id.itemfriend_textview);
+            }
+        }
+    }
+}
+
